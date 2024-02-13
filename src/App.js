b253524b1b6541ea4939521e840f6fe7b78913b1
Devon Lambert <devon@realityknights.com>
2024-02-13 13:29:56 -0600

    Adds additional baseline functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Chargers", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
-    <>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
       <Stats />
-    </>
-  )
+    </div>
+  );
 }
 
 function Logo()  {
@@ -14,16 +22,66 @@ function Logo()  {
 }
 
 function Form() {
-  return <div className="add-form">
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(), description, quantity, packed: false
+    }
+
+    initialItems.push(newItem)
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-  </div>
+    <select value={quantity} onChange={
+      (e) => setQuantity(Number(e.target.value))
+    }>
+      {Array.from({length: 20}, (_, i) => i+1).map((num) => 
+        (<option value={num} key={num}>{num}</option>)
+      )}
+    </select>
+    <input 
+      type="text" 
+      placeholder="Item..." 
+      value={description} 
+      onChange={(e) => setDescription(e.target.value)} 
+    />
+    <button>Add</button>
+  </form>
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  )
 }
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? 
+      { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   )
